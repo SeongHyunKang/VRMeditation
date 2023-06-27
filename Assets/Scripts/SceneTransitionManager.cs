8a8025135f037d53a8a24c39db7a0e98d7d62251
Seong Hyun Kang <ruanelroem@gmail.com>
2023-06-27 20:10:18 +0900

    Final Commit
    - Transcendance done!

---
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public FadeScreen fadeScreen;
+
+    public void GoToScene(int sceneIndex)
+    {
+        StartCoroutine(GoToSceneRoutine(sceneIndex));
+    }
+
+    IEnumerator GoToSceneRoutine(int sceneIndex)
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.fadeDuration);
+    }
+}
